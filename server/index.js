38bb7ebb503d70b5cd41f0959a643ae62ffcf327
Muhammad Farhan <gfarhan18@gmail.com>
2023-04-27 16:28:59 +0500

    connected the mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const colors = require('colors');
 require('dotenv').config();
 const schema = require('./schema/schema');
+const connectDB=require('./config/db')
 const port = process.env.PORT || 5000;
 const node_env = process.env.NODE_ENV;
 const {
@@ -8,6 +10,9 @@ const {
 } = require('express-graphql');
 
 const app = express();
+
+//conect DB
+connectDB();
 
 app.use(
     "/graphql",
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,6 +11,31 @@ const {
     GraphQLList
 } = require('graphql');
 
+//project type
+const ProjectType = new GraphQLObjectType({
+    name: "Project",
+    fields: () => ({
+        id: {
+            type: GraphQLID,
+        },
+        name: {
+            type: GraphQLString,
+        },
+        description: {
+            type: GraphQLString,
+        },
+        status: {
+            type: GraphQLString,
+        },
+        client: {
+            type: ClientType,
+            resolve(parent, args) {
+                return clients.find(client => client.id === parent.clientId)
+            },
+        },
+    }),
+});
+
 //client type
 
 const ClientType = new GraphQLObjectType({
@@ -31,10 +56,27 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        clients:{
-            type:new GraphQLList(ClientType),
-            resolve(parent,args) {
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
                 return clients;
+            }
+        },
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: {
+                id: {
+                    type: GraphQLID
+                }
+            },
+            resolve(parent, args) {
+                return projects.find(project => project.id === args.id);
             }
         },
         client: {
